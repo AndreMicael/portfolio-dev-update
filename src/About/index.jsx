f664777cb6c4df6f0325415e0d3220358ac62644
Andre Sampaio <andremicael@gmail.com>
2024-06-03 20:47:06 -0400

    Seção Sobre Mim: Texto e Imagens atualizados

--- src/About/index.jsx
+++ src/About/index.jsx
@@ -1,4 +1,5 @@
-import "./about.scss"
+import "./about.scss";
+import Avatar from "../assets/avatar.png";
 
 const About = () => {
   return (
@@ -7,11 +8,22 @@ const About = () => {
        <div className="about-text-container
       
        container">
-        <p className="">Mussum Ipsum, cacilds vidis litro abertis. Quem num gosta di mé, 
-            boa gentis num é. Praesent vel viverra nisi. Mauris aliquet 
-            nunc non turpis scelerisque, eget.
-            </p>
-        <img className="" src="https://via.placeholder.com/150x170" alt="placeholder" />
+        <p className="text-about-me">
+        Desde criança me destaco pela criatividade, e hoje, busco usar esse talento para trazer 
+        soluções e ideias ao mundo, utilizando as mais diversas ferramentas. 
+       <br/> <br/>
+         <p className="text-about-me">Assim que ingressei na <strong>Universidade Federal de Mato Grosso</strong>, no curso de <strong>Sistemas de Informação</strong>, 
+         comecei minha busca por aperfeiçoamento das minhas habilidades e aprendendi novas tecnologias para, enfim, realizar meu 
+         sonho de materializar ideias no mundo real.</p>
+         <br/>
+         <p className="text-about-me">No momento criativo, gosto de usar o principio KISS (Keep it Short and Simple), onde viso criar 
+         interfaces e ambientes focados na <strong>simplicidade e usabilidade</strong>. O segredo é evitar complexidades desnecessárias,
+          mas nunca ter medo delas. </p>
+          <br/>
+          <p className="text-about-me">Gosto de encarar cada desafio como uma oportunidade de <strong>crescimento e inovação</strong>, sempre buscando a melhor forma de transformar 
+          conceitos em realidades funcionais e impactantes.</p></p>
+           
+        <img className="profile" src={Avatar} alt="Foto de Perfil" /> 
 
        </div>
     </div>
