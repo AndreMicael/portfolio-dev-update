e293ed5a04a445c36d0cea281e502b010402410b
Andre Sampaio <andremicael@gmail.com>
2024-06-27 21:18:27 -0400

    Aba projetos melhorada

--- src/assets/ProjectMockup.jsx
+++ src/assets/ProjectMockup.jsx
@@ -1,15 +1,14 @@
 import "../Projects/projects.scss";
 import { TbWorld } from "react-icons/tb";
 import { FaGithub } from "react-icons/fa6";
-import { FaHtml5, FaCss3Alt } from "react-icons/fa6";
-import { RiJavascriptFill } from "react-icons/ri";
+
 
 const ProjectMockup = ({ project }) => {
   const { titulo, desc, img, stack,link,github } = project;
 
   return (
     <div className="grid grid-cols-6 mockup">
-      <div className="container img-container rounded-2xl overflow-hidden h-[50vh] col-span-4
+      <div className="container relative img-container rounded-2xl overflow-hidden h-[50vh] col-span-4
       
       ">
         <img className='rounded-lg' src={img} alt={desc} />
@@ -17,7 +16,7 @@ const ProjectMockup = ({ project }) => {
 
       <div className="col-span-2 pl-8 text-justify flex flex-col gap-4">
         <p className="text-2xl text-verde font-semibold">{titulo}</p>
-        <div className="project-desc">{desc}</div>
+        <div className="project-desc text-sm2">{desc}</div>
         <div className="flex justify-center gap-5">
          <a href={link} target="blank"><button className="botao-projeto px-5 py-2 rounded-3xl flex gap-1 items-center hover:bg-gray-700">
             <div className="text-lg text-cyan-500"><TbWorld /></div>
--- src/assets/texts.jsx
+++ src/assets/texts.jsx
@@ -1,5 +1,6 @@
 import React from 'react';
 import Projeto1 from "../assets/projeto1.png";
+import Projeto2 from "../assets/projeto2.png";
 import { FaHtml5 } from "react-icons/fa6";
 import { FaCss3Alt,FaReact  } from "react-icons/fa";
 import { RiJavascriptFill ,RiJavaLine} from "react-icons/ri";
@@ -15,7 +16,7 @@ const Springboot = <div className='text-green-500'><BiLogoSpringBoot /></div>;
 
 export const getProject = [
   { id: 1, titulo: 'Ion Imóveis', desc: "A página tem como principal objetivo captar potenciais clientes para a imobiliária, fornecendo informações detalhadas sobre o empreendimento e incentivando a conversão através de formulários de contato e agendamentos de visitas.", stack: [HTML, CSS, Javascript], img: Projeto1, link: 'https://andremicael.github.io/Ion-Imoveis-LandingPage/',github:'https://github.com/AndreMicael/Ion-Imoveis-LandingPage' },
-  { id: 2, titulo: 'My Book API / Site', desc: "Mussum Ipsum, cacilds vidis litro abertis. Nulla id gravida magna, ut semper sapien.Em pé sem cair, deitado sem dormir, sentado sem cochilar e fazendo pose.", stack: [HTML, CSS,ReactJs,Java,Springboot], img: "https://via.placeholder.com/1920x1080" },
+  { id: 2, titulo: 'My Book API / Site', desc: "Mussum Ipsum, cacilds vidis litro abertis. Nulla id gravida magna, ut semper sapien.Em pé sem cair, deitado sem dormir, sentado sem cochilar e fazendo pose.", stack: [HTML, CSS,ReactJs,Java,Springboot], img: Projeto2 },
   { id: 3, titulo: 'Ion Imóveis', desc: "Mussum Ipsum, cacilds vidis litro abertis. Nulla id gravida magna, ut semper sapien.Em pé sem cair, deitado sem dormir, sentado sem cochilar e fazendo pose.", stack: [HTML, CSS], img: "https://via.placeholder.com/1920x1080" },
   { id: 4, titulo: 'Ion Imóveis', desc: "Mussum Ipsum, cacilds vidis litro abertis. Nulla id gravida magna, ut semper sapien.Em pé sem cair, deitado sem dormir, sentado sem cochilar e fazendo pose.", stack: [HTML, CSS], img: "https://via.placeholder.com/1920x1080" },
 ];
