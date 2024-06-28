811fd3dd402862b4ac76b34e8396082b689d0485
Andre Sampaio <andremicael@gmail.com>
2024-06-28 13:01:12 -0400

    Responsividade aba projetos

--- src/assets/ProjectMockup.jsx
+++ src/assets/ProjectMockup.jsx
@@ -7,16 +7,22 @@ const ProjectMockup = ({ project }) => {
   const { titulo, desc, img, stack,link,github } = project;
 
   return (
-    <div className="grid grid-cols-6 mockup">
-      <div className="container relative img-container rounded-2xl overflow-hidden h-[50vh] col-span-4
+    <div className="grid grid-cols-6 mockup ">
+      <div className="container relative img-container rounded-2xl overflow-hidden h-[50vh] 
+      
+      xl:col-span-4 lg:col-span-3 md:col-span-6 sm:col-span-4 col-span-6 
+     
       
       ">
         <img className='rounded-lg' src={img} alt={desc} />
       </div>
 
-      <div className="col-span-2 pl-8 text-justify flex flex-col gap-4">
+      <div className="col-span-2 text-justify flex flex-col gap-4
+       xl:col-span-4 lg:col-span-3 md:col-span-6 sm:col-span-4 col-span-6
+       xl: pl-8 lg:pl-8 sm:pl-0 md:pl-0 pl-0
+      ">
         <p className="text-2xl text-verde font-semibold">{titulo}</p>
-        <div className="project-desc text-sm2">{desc}</div>
+        <div className="project-desc text-sm2 sm:mt-[-5vw] md:mt-[-3vw]">{desc}</div>
         <div className="flex justify-center gap-5">
          <a href={link} target="blank"><button className="botao-projeto px-5 py-2 rounded-3xl flex gap-1 items-center hover:bg-gray-700">
             <div className="text-lg text-cyan-500"><TbWorld /></div>
