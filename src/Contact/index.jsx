e80de82813a5e5f6566c74a37dac223c69db664a
Andre <andremicael@gmail.com>
2024-08-01 14:03:58 -0400

    Formulario contato

--- src/Contact/index.jsx
+++ src/Contact/index.jsx
@@ -46,7 +46,7 @@ xl:w-[30vw] lg:w-[40vw] md:w-[50vw] sm:w-[60vw] xs:w-[70vw]  dark:bg-gray-800 da
     <span className="inline-flex items-center px-3 text-sm text-gray-900 bg-gray-200 border border-e-0 border-gray-300 rounded-s-md dark:bg-gray-600 dark:text-gray-400 dark:border-gray-600">
     <FaUserCircle />
     </span>
-    <input type="text" id="name" className="rounded-none rounded-e-lg bg-gray-50 border border-gray-300 text-gray-900 focus:ring-blue-500 focus:border-blue-500 block 
+    <input  type="text" name="name" id="name" className="rounded-none rounded-e-lg bg-gray-50 border border-gray-300 text-gray-900 focus:ring-blue-500 focus:border-blue-500 block 
     flex-1 min-w-0 w-full text-sm p-2.5  dark:bg-gray-700 dark:border-gray-600 dark:placeholder-gray-400 dark:text-white dark:focus:ring-blue-500 dark:focus:border-blue-500"
      placeholder="José Pereira"/>
     
@@ -64,7 +64,7 @@ xl:w-[30vw] lg:w-[40vw] md:w-[50vw] sm:w-[60vw] xs:w-[70vw]  dark:bg-gray-800 da
 
  
 
-<textarea id="message" rows="4" className="block p-2.5 w-full text-sm text-gray-900 bg-gray-50 rounded-lg border border-gray-300 focus:ring-blue-500 focus:border-blue-500 m-0
+<textarea required id="message" rows="4" name="message"  className="form-control block p-2.5 w-full text-sm text-gray-900 bg-gray-50 rounded-lg border border-gray-300 focus:ring-blue-500 focus:border-blue-500 m-0
  dark:bg-gray-700 dark:border-gray-600 dark:placeholder-gray-400 dark:text-white dark:focus:ring-blue-500 dark:focus:border-blue-500" placeholder="Escreva sua mensagem aqui..."></textarea>
 <button  type="submit" className="focus:outline-none text-preto bg-verde hover:bg-verde-shadow focus:ring-4 focus:ring-green-300 font-medium rounded-md text-sm px-5 py-2.5  dark:bg-green-600 dark:hover:bg-green-700 dark:focus:ring-green-800">Enviar</button>
 {/* <input type="hidden" name="_next" value=''/> Inserir página de voltar*/} 
