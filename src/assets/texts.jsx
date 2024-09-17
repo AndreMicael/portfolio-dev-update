493d240bca8c7b5ea9e29219685b8167b2c1391e
Andre <andremicael@gmail.com>
2024-09-17 13:38:36 -0400

    Melhorado texto projeto 2

--- src/assets/texts.jsx
+++ src/assets/texts.jsx
@@ -21,7 +21,7 @@ const NextJS = <div className='text-purple-500'><RiNextjsFill /></div>
 
 export const getProject = [
   { id: 1, titulo: 'Ion Imóveis', desc: "A página tem como principal objetivo captar potenciais clientes para a imobiliária, fornecendo informações detalhadas sobre o empreendimento e incentivando a conversão através de formulários de contato e agendamentos de visitas.", stack: [HTML, CSS, Javascript], img: Projeto1, link:'https://andremicael.github.io/Ion-Imoveis-LandingPage/',github:'https://github.com/AndreMicael/Ion-Imoveis-LandingPage' },
-  { id: 2, titulo: 'Stock Wall', desc: "Projeto criado para analisar ações da Bolsa de Valores Brasileira. Os valores e indicativos são adquiridos por meio de webscrapping (Puppeteer), e enviados para o front-end onde será analizado.", stack: [HTML, CSS,ReactJs,Puppeteer,NextJS], img: Projeto2,github:'https://github.com/AndreMicael/stock-wall', link:'https://stock-wall.vercel.app/' },
+  { id: 2, titulo: 'Stock Wall', desc: "Projeto criado para analisar ações da Bolsa de Valores Brasileira. Os dados foram adquiridos, colocados no banco de dados MongoDB e enviados para o front-end onde serão analizados.", stack: [HTML, CSS,ReactJs,Puppeteer,NextJS], img: Projeto2,github:'https://github.com/AndreMicael/stock-wall', link:'https://stock-wall.vercel.app/' },
   
  
 ];
