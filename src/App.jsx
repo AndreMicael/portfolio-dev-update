9e4bb17c2ead1579da1699baaa87ba20793a801f
Andre <andremicael@gmail.com>
2025-03-04 19:36:34 -0400

    fix: htaccess

--- src/App.jsx
+++ src/App.jsx
@@ -1,4 +1,4 @@
-import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
+import { HashRouter as Router, Routes, Route } from "react-router-dom";
 import React, { useEffect, useState } from "react";
 import ParticlesComponent from "../src/components/particles";
 import { HomeTabsProvider } from "./Contexto/HomeTabsContext";
